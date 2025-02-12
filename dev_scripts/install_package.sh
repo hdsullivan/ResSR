d7922d3f2e9a03886d79d1a2e99842688a4fb45c
hdsullivan <sullivanhe@ornl.gov>
2025-02-12 13:09:42 -0500

    Update dev scripts

--- dev_scripts/install_package.sh
+++ dev_scripts/install_package.sh
@@ -8,7 +8,6 @@ conda install -c conda-forge gdal
 cd ..
 pip install -r requirements.txt
 pip install -e .
-pip install -r demo/requirements.txt
 pip install -r docs/requirements.txt 
 cd dev_scripts
 
